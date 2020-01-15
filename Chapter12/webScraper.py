32dd53af5c375a5e25b2ce4f307e0f3d98c593d8
Merckens <Jeffery.Merckens@maine.gov>
2020-01-15 08:15:15 -0500

    Add notes for webScraper plan

--- Chapter12/webScraper.py
+++ Chapter12/webScraper.py
@@ -5,9 +5,15 @@ res.raise_for_status()
 noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
 events = noStarchSoup.select('.entry-header')
 
-eventText = []
-for item in events:
-    eventText.append(item.getText())
+eventText = [ item.getText() for item in events ]
 
 for event in eventText:
     print(event)
+
+'''
+Compare first event scraped to first event saved,
+    if they're the same then move along,
+    if they're different then see if the scraped event is saved (as not first),
+        if so, one was inserted beforehand,
+        if not, it must've been inserted
+'''
